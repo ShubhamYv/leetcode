4a66174645f9905108a6d6d049996da7b136849c
Shubham Kumar Yadav <75082349+ShubhamYv@users.noreply.github.com>
2022-09-17 08:43:12 +0530

    Create 704. Binary Search.py

---
+++ 704. Binary Search.py
@@ -0,0 +1,14 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        low=0
+        high=len(nums)-1
+            
+        while low <= high:  
+            mid= (high+ low)//2
+            if nums[mid]== target:
+                return mid
+            elif nums[mid]> target:
+                high= mid-1
+            else:
+                low= mid+1
+        return -1
