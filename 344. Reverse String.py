22734b2b9b9fce91f8fcf14964c63203259cc709
Shubham Kumar Yadav <75082349+ShubhamYv@users.noreply.github.com>
2022-09-20 20:26:52 +0530

    Create 344. Reverse String.py

---
+++ 344. Reverse String.py
@@ -0,0 +1,8 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        i, j= 0, len(s)-1
+        while i< j:
+            s[i], s[j]= s[j], s[i]
+            i+=1
+            j-=1
+        return s
